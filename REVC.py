5b478123c3ee495e1a553a69f7069d10c5d8a414
Shashank H S <45012110+shashankhs29@users.noreply.github.com>
2019-05-24 21:26:09 +0530

    Complementing a Strand of DNA (Reverse Complement)
    Full question at : http://rosalind.info/problems/revc/

---
+++ REVC.py
@@ -0,0 +1,15 @@
+from string import whitespace
+def revcomp(seq):
+    nt = ['A', 'T', 'G', 'C']
+    compdict = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    seqlist = [x for x in seq if x not in whitespace and x in nt]
+    seqrev = ''.join(reversed(seqlist))
+    seqrc = ""
+    for i in seqrev:
+        seqrc+=compdict[i]
+    return seqrc
+
+fi = open("rosalind_revc.txt", "r")
+strand = fi.read()
+print("Reverse compliment of given DNA strand:\n")
+print(revcomp(strand))
