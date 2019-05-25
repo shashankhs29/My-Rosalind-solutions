210a9b01e2847bc94511cfac94bad7aecfacda1e
Shashank H S <45012110+shashankhs29@users.noreply.github.com>
2019-05-25 22:45:58 +0530

    Rabbits and Recurrence Relations (Dynamic programming)
    Full question at : http://rosalind.info/problems/fib/

---
+++ FIB.py
@@ -0,0 +1,18 @@
+def fib(n, k):
+    #The answer is x(n) = f(n-1) + k*f(n-2)
+    #This can be done using dynamic programming
+    
+    arr = [0, 1]
+    for i in range(2, n+1):
+        next = arr[1] + k*arr[0]
+        arr[0] = arr[1]
+        arr[1] = next
+    return arr[1]
+    
+fi = open("rosalind_fib.txt", "r")
+a = fi.read()
+b = [int(x) for x in a.split()]
+n = b[0]
+k = b[1]
+print(fib(n, k))
+fi.close()
