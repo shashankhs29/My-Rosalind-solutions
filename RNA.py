b42940fc23655f17f306b8ff51a83677b0322fe9
Shashank H S <45012110+shashankhs29@users.noreply.github.com>
2019-05-26 17:30:42 +0530

    Transcribing DNA (Coding strand) into RNA
    Full question at : http://rosalind.info/problems/rna/

---
+++ RNA.py
@@ -0,0 +1,14 @@
+from string import whitespace
+def transcribe(seq):
+    nt = ['A','T', 'G', 'C']
+    seqlist = [x for x in seq if x not in whitespace]
+    s1 = [y for y in seqlist if y in nt]
+    s2 = ''.join(s1)
+    rna = s2.replace('T', 'U')
+    return rna
+
+fi = open("rosalind_rna.txt", "r")
+strand = fi.read()
+print("Transcribed RNA strand:\n")
+print(transcribe(strand))
+fi.close()
